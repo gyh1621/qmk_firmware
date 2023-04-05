084213ecbbed468674009bf3ddd46932c2aae45f
gyh1621 <cngyh96@gmail.com>
2023-04-05 10:01:02 -0700

    dz60_wkl: arrow keys

---
+++ keyboards/dztech/dz60rgb_wkl/keymaps/2_1_1621/keymap.c
@@ -0,0 +1,48 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT_60_tsangan_hhkb(
+        QK_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL,
+        KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_LSFT,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MO(2), MO(1),
+        KC_LCTL,        KC_LALT, KC_LGUI,                            KC_SPC,                                      KC_RGUI, KC_RALT,   KC_RCTL
+    ),
+    [1] = LAYOUT_60_tsangan_hhkb(
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
+        _______, _______, KC_UP,   _______, _______, _______, KC_CALC, _______, _______,  _______, _______, KC_UP, _______, _______,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_RGHT,          _______,
+        KC_MPRV,          KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, NK_TOGG, _______, _______, _______,  KC_DOWN, _______, _______,
+        _______, _______, _______,                            _______,                                     _______, _______, _______
+    ),
+    [2] = LAYOUT_60_tsangan_hhkb(
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  _______,
+        _______, RGB_TOG, _______, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_MOD, _______, _______, _______, QK_BOOT,
+        _______, _______, _______, _______, _______, _______, _______, _______, RGB_SPI, RGB_SPD, _______, _______,          _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______,                            _______,                                     _______, _______, _______
+    )
+};
+
+// Ctrl + Backspace = Delete
+const key_override_t enter_key_override = ko_make_basic(MOD_MASK_CTRL, KC_LGUI, KC_ENT);
+
+// Ctrl + h = left_arrow
+const key_override_t ctrl_h_key_override = ko_make_basic(MOD_MASK_CTRL, KC_H, KC_LEFT);
+// Ctrl + l = right_arrow
+const key_override_t ctrl_l_key_override = ko_make_basic(MOD_MASK_CTRL, KC_L, KC_RIGHT);
+// Ctrl + j = down_arrow
+const key_override_t ctrl_j_key_override = ko_make_basic(MOD_MASK_CTRL, KC_J, KC_DOWN);
+// Ctrl + k = up_arrow
+const key_override_t ctrl_k_key_override = ko_make_basic(MOD_MASK_CTRL, KC_K, KC_UP);
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+	&enter_key_override,
+	&ctrl_h_key_override,
+	&ctrl_l_key_override,
+	&ctrl_j_key_override,
+	&ctrl_k_key_override,
+	NULL // Null terminate the array of overrides!
+};
+
