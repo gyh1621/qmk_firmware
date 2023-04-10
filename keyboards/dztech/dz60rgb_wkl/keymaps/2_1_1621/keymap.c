0208d88ac1858b5078148e497a1f975600a40b48
gyh1621 <cngyh96@gmail.com>
2023-04-09 21:25:21 -0700

    override enter

--- keyboards/dztech/dz60rgb_wkl/keymaps/2_1_1621/keymap.c
+++ keyboards/dztech/dz60rgb_wkl/keymaps/2_1_1621/keymap.c
@@ -2,7 +2,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_60_tsangan_hhkb(
-        QK_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL,
+        QK_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRAVE,
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MO(2), MO(1),
@@ -26,6 +26,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Ctrl + ; = Enter
 const key_override_t enter_key_override = ko_make_basic(MOD_MASK_CTRL, KC_SEMICOLON, KC_ENT);
+// CMD + b = Enter
+const key_override_t enter_key_override_1 = ko_make_basic(MOD_MASK_GUI, KC_B, KC_ENT);
 
 // Ctrl + b = Backspace
 const key_override_t backspace_key_override = ko_make_basic(MOD_MASK_CTRL, KC_B, KC_BACKSPACE);
@@ -42,6 +44,7 @@ const key_override_t ctrl_k_key_override = ko_make_basic(MOD_MASK_CTRL, KC_K, KC
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
 	&enter_key_override,
+    &enter_key_override_1,
 	&backspace_key_override,
 	&ctrl_h_key_override,
 	&ctrl_l_key_override,
