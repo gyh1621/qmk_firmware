cd2bc2ca60030557ab8399e1cd7672bd6fb80d9f
gyh1621 <cngyh96@gmail.com>
2023-11-12 23:07:28 -0800

    vault35: enable caps word & autocorrect

--- keyboards/projectcain/vault35/keymaps/gyh1621-112211/keymap.c
+++ keyboards/projectcain/vault35/keymaps/gyh1621-112211/keymap.c
@@ -124,16 +124,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [NUM] = LAYOUT_split_4space(
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_TAB, KC_MINUS, KC_EQL,  KC_BSLS, KC_SLSH, KC_GRV,   KC_QUOT, KC_SCLN, KC_LBRC, KC_RBRC,
     KC_LSFT, CTRL_Z,   ALT_L,   CTRL_A,  KC_BSPC, CTRL_S,   KC_NO,   KC_NO,   KC_NO,   OSM(MOD_RSFT),
-            SPOTLIGHT, SPOTLIGHT,       KC_TAB, KC_TAB,     KC_TRNS, KC_TRNS
+            QK_CAPS_WORD_TOGGLE, SPOTLIGHT,       KC_TAB, KC_TAB,     KC_TRNS, KC_TRNS
   ),
 
   [SYM] = LAYOUT_split_4space(
     KC_ESC,    KC_F7,  KC_F8,  KC_F9,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_UP,    CTRL_LEFT,  CTRL_RIGHT,  KC_TRNS,
     KC_TAB,    KC_F4,  KC_F5,  KC_F6,  KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_RIGHT, KC_TRNS,  KC_TRNS,
     KC_LSFT,   KC_F1,  KC_F2,  KC_F3,  KC_KB_VOLUME_DOWN,  KC_KB_VOLUME_UP,  KC_DOWN,  KC_PGDN,  KC_PGUP,  KC_TRNS,
-            KC_LALT,   KC_LGUI,         KC_TRNS, KC_TRNS,    KC_TRNS, QK_BOOT
+            KC_LALT,   KC_LGUI,         KC_TRNS, KC_TRNS,    AC_TOGG, QK_BOOT
   ),
 };
